6402990447ecc1c3499a877cc162d8316e93741a
Vadim Majsuk <vmajsuk@gmail.com>
2021-04-11 17:46:58 +0300

    chore: cleaned up, updated output format

--- index.ts
+++ index.ts
@@ -1,63 +1,25 @@
-import * as core from '@actions/core'
-import * as github from '@actions/github'
-import Octokit = require('@octokit/rest')
+import * as core from "@actions/core";
+import * as github from "@actions/github";
 
-const token: string = core.getInput('token')
-const labels: string[] = JSON.parse(core.getInput('labels'))
-const skipSec: Number = parseInt(core.getInput('skip_hour')) * 60 * 60
-const repoOwner: string = github.context.repo.owner
-const repo: string = github.context.repo.repo
+const token: string = core.getInput("token");
+const label: string = core.getInput("label");
 
-function pullRequests(repoOwner:string, repo:string ):Promise<Octokit.Response<Octokit.PullsListResponse>> {
-    let pr = new github.GitHub(token)
-    let resp = pr.pulls.list({
-        owner: repoOwner,
-        repo: repo,
-    }).catch(
-        e => {
-            console.log(e.message)
-        }
-    ) as Promise<Octokit.Response<Octokit.PullsListResponse>>
-    return resp
-}
+const octokit = github.getOctokit(token);
 
-function filterLabel(labels: Octokit.PullsListResponseItemLabelsItem[],target: string[]):boolean{
-    let labelname = labels.map((label) => {
-        return label.name
+async function run() {
+  const prs = await octokit.pulls
+    .list({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
     })
-    let filterdLabels = labelname.filter(
-        label => target.indexOf(label) != -1
-    )
-    if ( filterdLabels.length == target.length) {
-        return true
-    } else {
-        return false
-    }
+    .then((r) => r.data);
+
+  const prsWirthLabel = prs.filter((pr) =>
+    pr.labels.find(({ name }) => name === label)
+  );
+
+  const output = prsWirthLabel.map((pr) => pr.html_url).join("\n");
+  core.setOutput("prs", output);
 }
 
-function filterTime(pull: Octokit.PullsListResponseItem,target: number):boolean{
-    const createdAt = Date.parse(pull.created_at)
-    const gapSec = Math.round((target - createdAt) / 1000)
-    if ( gapSec > skipSec ) {
-        return true
-    }
-    return false
-}
-
-function setOutput(pull:Octokit.PullsListResponseItem[]){
-    let output = ''
-    for (const p of pull) {
-        output = output + p.title + "\\n" + p.html_url + "\\n---\\n"
-    }
-    output = output.slice(0,-7) //最後の"\\n---\\n"を削除
-    core.setOutput('pulls', output)
-}
-
-const now = Date.now()
-const prom = pullRequests(repoOwner,repo)
-prom.then((pulls) => {
-    let claim = pulls.data.filter(
-        p => filterLabel(p.labels, labels) && filterTime(p ,now)
-    )
-    setOutput(claim)
-})
+run();
